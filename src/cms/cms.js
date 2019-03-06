b0fc3a46bfaf8930892828c94bf978db4077ae6f
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2019-03-06 02:35:39 -0800

    Added Past Meetups page, template, and component. Updated admin panel config to reflect change.
    Signed-off-by: Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,15 @@
 import CMS from "netlify-cms"
 
-import AboutPagePreview from "./preview-templates/AboutPagePreview"
-import HomePagePreview from "./preview-templates/HomePagePreview"
+import AboutPagePreview from "./preview-templates/AboutPreview"
+import HomePagePreview from "./preview-templates/HomePreview"
 import MeetupPreview from "./preview-templates/MeetupPreview"
 import FooterPreview from "./preview-templates/FooterPreview"
 import NavbarPreview from "./preview-templates/NavbarPreview"
+import PastMeetupsPagePreview from "./preview-templates/PastMeetupsPreview"
 
 CMS.registerPreviewTemplate("meetups", MeetupPreview)
 CMS.registerPreviewTemplate("footer", FooterPreview)
 CMS.registerPreviewTemplate("navbar", NavbarPreview)
 CMS.registerPreviewTemplate("about", AboutPagePreview)
 CMS.registerPreviewTemplate("home", HomePagePreview)
+CMS.registerPreviewTemplate("past-meetups", PastMeetupsPagePreview)
