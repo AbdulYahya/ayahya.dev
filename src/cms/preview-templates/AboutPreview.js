789a9ee6e3d893c8b00f5855969e06d8ef6fa857
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2019-03-07 00:08:23 -0800

    updated aboutTemplate to match defined templateKey.
    Signed-off-by: Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>

--- src/cms/preview-templates/AboutPreview.js
+++ src/cms/preview-templates/AboutPreview.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { AboutPageTemplate } from "../../templates/aboutTemplate"
+import { AboutPageTemplate } from "../../templates/about-page"
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
