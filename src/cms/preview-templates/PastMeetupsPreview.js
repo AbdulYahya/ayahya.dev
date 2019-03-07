eafc17f534917b4984c6f943f0dde43572e3347e
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2019-03-07 00:14:11 -0800

    Updated pastMeetupsTemplate to match defined templateKey.
    Signed-off-by: Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>

--- src/cms/preview-templates/PastMeetupsPreview.js
+++ src/cms/preview-templates/PastMeetupsPreview.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { PastMeetupsPageTemplate } from "../../templates/pastMeetupsTemplate"
+import { PastMeetupsPageTemplate } from "../../templates/past-meetups-page"
 
 const PastMeetupsPagePreview = ({ entry, widgetFor }) => {
   return (
