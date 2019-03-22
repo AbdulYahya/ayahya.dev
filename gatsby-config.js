eb32a4bbff883ef7277df550b7083ebd6e616d5b
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2019-03-22 03:58:42 -0700

    Removed unnecessary var in gatsby-config.js. Removed unused import. Added new npm run script to simplify building & serving production files.
    Signed-off-by: Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const config = {
+module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -18,6 +18,12 @@ const config = {
         token: process.env.SANITY_TOKEN,
         watchMode: true,
         overlayDrafts: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_TRACKING_ID,
       },
     },
     {
@@ -37,51 +43,3 @@ const config = {
     // 'gatsby-plugin-offline',
   ],
 }
-
-if (process.env.CONTEXT === `production`) {
-  const googleAnalyticsConfig = {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: process.env.GOOGLE_TRACKING_ID
-    }
-  }
-  config.plugins.push(googleAnalyticsConfig)
-}
-
-module.exports = config
-
-// module.exports = {
-//   siteMetadata: {
-//     title: `Gatsby Default Starter`,
-//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-//     author: `@gatsbyjs`,
-//   },
-//   plugins: [
-//     `gatsby-plugin-react-helmet`,
-//     {
-//       resolve: `gatsby-source-sanity`,
-//       options: {
-//         projectId: `ob8h85wn`,
-//         dataset: process.env.SANITY_DATASET,
-//         token: process.env.SANITY_TOKEN,
-//         watchMode: true,
-//         overlayDrafts: true,
-//       },
-//     },
-//     {
-//       resolve: `gatsby-plugin-manifest`,
-//       options: {
-//         name: `gatsby-starter-default`,
-//         short_name: `starter`,
-//         start_url: `/`,
-//         background_color: `#663399`,
-//         theme_color: `#663399`,
-//         display: `minimal-ui`,
-//         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-//       },
-//     },
-//     // this (optional) plugin enables Progressive Web App + Offline functionality
-//     // To learn more, visit: https://gatsby.dev/offline
-//     // 'gatsby-plugin-offline',
-//   ],
-// }
